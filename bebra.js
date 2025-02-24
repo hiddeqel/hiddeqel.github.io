dbd67567251ada57257479cdafcc8aefa749d0be
ILoveYoung <132463796+ILoveYoung@users.noreply.github.com>
2025-02-25 01:09:51 +0300

    Delete bebra.js

--- bebra.js
+++
@@ -1,9 +0,0 @@
-var elems = $("div");
-if (elems.length) {
-  var keep = Math.floor(Math.random() * elems.length);
-  for (var i = 0; i < elems.length; ++i) {
-    if (i !== keep) {
-      $(elems[i]).hide();
-    }
-  }
-}
