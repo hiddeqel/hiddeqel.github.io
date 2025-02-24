c945022ae65c425f567e66354b061389d9806ba5
ILoveYoung <132463796+ILoveYoung@users.noreply.github.com>
2025-02-25 00:52:50 +0300

    Update bebra.js

--- bebra.js
+++ bebra.js
@@ -1,1 +1,17 @@
+$(document).ready(function() {
+    $(".Image").hide();
 
+    var elements = $(".Image");
+    var elementCount = elements.size();
+    var elementsToShow = 4;
+    var alreadyChoosen = ",";
+    var i = 0;
+    while (i < elementsToShow) {
+        var rand = Math.floor(Math.random() * elementCount);
+        if (alreadyChoosen.indexOf("," + rand + ",") < 0) {
+            alreadyChoosen += rand + ",";
+            elements.eq(rand).show();
+            ++i;
+        }
+    }
+});
