747ec8c7c19fcaa28dbfde857ec33cc4d6666501
ILoveYoung <132463796+ILoveYoung@users.noreply.github.com>
2025-02-25 00:57:01 +0300

    Update bebra.js

--- bebra.js
+++ bebra.js
@@ -1,17 +1,4 @@
-$(document).ready(function() {
-    $(".Image").hide();
-
-    var elements = $(".Image");
-    var elementCount = elements.size();
-    var elementsToShow = 4;
-    var alreadyChoosen = ",";
-    var i = 0;
-    while (i < elementsToShow) {
-        var rand = Math.floor(Math.random() * elementCount);
-        if (alreadyChoosen.indexOf("," + rand + ",") < 0) {
-            alreadyChoosen += rand + ",";
-            elements.eq(rand).show();
-            ++i;
-        }
-    }
-});
+var divs = $("div.Image").get().sort(function(){ 
+  return Math.round(Math.random())-0.5; //random so we get the right +/- combo
+}).slice(0,4)
+$(divs).show();
